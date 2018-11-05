97f985330df4cce731179678e7a580917a4aa2f8
w11ld33r <w11ld33r@gmail.com>
2018-11-05 08:01:35 -0500

    Commit inicial

---
+++ src/main/java/com/example/FileApp.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileApp.class, args);
+	}
+}
